b5cf4effdd12af1bd40d07c2221375cd70917a77
hjz888 <2702794@163.com>
2020-09-10 15:37:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ clone_repo(){
     rm "$TMP_FILE"
     echo "当前最新V2Ray版本为$RELEASE_LATEST"
     # Download latest release
-    DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/$RELEASE_LATEST/v2ray-linux-64.zip"
+    DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/v4.27.5/v2ray-linux-64.zip"
     if ! curl -L -H 'Cache-Control: no-cache' -o "latest-v2ray.zip" "$DOWNLOAD_LINK"; then
         echo 'error: 下载V2Ray失败，请重试'
         return 1
